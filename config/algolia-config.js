5ec42868c43cfa057c8065863e02b9a7d6a455c4
omermorad <omer.moradd@gmail.com>
2024-07-07 07:20:44 +0300

    set search engine

--- config/algolia-config.js
+++ config/algolia-config.js
@@ -1,105 +1,12 @@
 module.exports = {
   appId: 'S3EVZGZBUX',
   apiKey: '35d82ebfb36fc4302594525adbcfa45e',
+  indexName: 'netlify_8963435b-d90c-4317-801d-f102b0f669bc_master_all',
   rateLimit: 8,
   maxDepth: 10,
-  startUrls: ['https://suites.dev/', 'https://suites.dev/api-reference', 'https://suites.dev/key-features', 'https://suites.dev/docs'],
+  startUrls: ['https://suites.dev', 'https://suites.dev/docs'],
   sitemaps: ['https://suites.dev/sitemap.xml'],
   ignoreCanonicalTo: true,
-  discoveryPatterns: ['https://suites.dev/**'],
-  indexName: 'netlify_8963435b-d90c-4317-801d-f102b0f669bc_master_all',
-  searchPagePath: 'search',
-  actions: [
-    {
-      indexName: 'netlify_8963435b-d90c-4317-801d-f102b0f669bc_master_all',
-      pathsToMatch: ['https://suites.dev/**'],
-      recordExtractor: ({ $, helpers }) => {
-        // priority order: deepest active sub list header -> navbar active item -> 'Documentation'
-        const lvl0 =
-          $(
-            '.menu__link.menu__link--sublist.menu__link--active, .navbar__item.navbar__link--active'
-          )
-            .last()
-            .text() || 'Documentation';
-
-        return helpers.docsearch({
-          recordProps: {
-            lvl0: {
-              selectors: '',
-              defaultValue: lvl0,
-            },
-            lvl1: ['header h1', 'article h1'],
-            lvl2: 'article h2',
-            lvl3: 'article h3',
-            lvl4: 'article h4',
-            lvl5: 'article h5, article td:first-child',
-            lvl6: 'article h6',
-            content: 'article p, article li, article td:last-child',
-          },
-          indexHeadings: true,
-          aggregateContent: true,
-          recordVersion: 'v3',
-        });
-      },
-    },
-  ],
-  initialIndexSettings: {
-    suites: {
-      attributesForFaceting: [
-        'type',
-        'lang',
-        'language',
-        'version',
-        'docusaurus_tag',
-      ],
-      attributesToRetrieve: [
-        'hierarchy',
-        'content',
-        'anchor',
-        'url',
-        'url_without_anchor',
-        'type',
-      ],
-      attributesToHighlight: ['hierarchy', 'content'],
-      attributesToSnippet: ['content:10'],
-      camelCaseAttributes: ['hierarchy', 'content'],
-      searchableAttributes: [
-        'unordered(hierarchy.lvl0)',
-        'unordered(hierarchy.lvl1)',
-        'unordered(hierarchy.lvl2)',
-        'unordered(hierarchy.lvl3)',
-        'unordered(hierarchy.lvl4)',
-        'unordered(hierarchy.lvl5)',
-        'unordered(hierarchy.lvl6)',
-        'content',
-      ],
-      distinct: true,
-      attributeForDistinct: 'url',
-      customRanking: [
-        'desc(weight.pageRank)',
-        'desc(weight.level)',
-        'asc(weight.position)',
-      ],
-      ranking: [
-        'words',
-        'filters',
-        'typo',
-        'attribute',
-        'proximity',
-        'exact',
-        'custom',
-      ],
-      highlightPreTag: '<span class="algolia-docsearch-suggestion--highlight">',
-      highlightPostTag: '</span>',
-      minWordSizefor1Typo: 3,
-      minWordSizefor2Typos: 7,
-      allowTyposOnNumericTokens: false,
-      minProximity: 1,
-      ignorePlurals: true,
-      advancedSyntax: true,
-      attributeCriteriaComputedByMinProximity: true,
-      removeWordsIfNoResults: 'allOptional',
-      separatorsToIndex: '_',
-    },
-  },
+  discoveryPatterns: ['https://suites.dev/docs/**'],
+  searchPagePath: 'search'
 };
