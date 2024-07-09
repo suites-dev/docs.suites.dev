e134198a5511fbb006ad47e6834bd88ded4c07c5
omermorad <omer.moradd@gmail.com>
2024-07-09 23:27:11 +0300

    meta tags og

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -50,12 +50,13 @@ const config: Config = {
         name: 'keywords',
         content: 'auto mocking, meta framework, jest, sinon, vitest, dependency injection, inversion of control, nestjs, inversifyjs'
       },
-      { name: 'description', content: 'A meta-framework that focuses on helping developers build solid test suites, eliminates boilerplate code, and improves their unit testing process.' },
-      { name: 'og:title', content: 'Suites' },
-      { name: 'og:description', content: 'A meta-framework that focuses on helping developers build solid test suites, eliminates boilerplate code, and improves their unit testing process.' },
-      { name: 'og:image', content: 'https://suites.dev/img/banner.png' },
-      { name: 'og:url', content: 'https://suites.dev' },
-      { name: 'og:type', content: 'website' }
+      { name: 'author', content: 'Suites' },
+      { property: 'description', content: 'A meta-framework that focuses on helping developers build solid test suites, eliminates boilerplate code, and improves their unit testing process.' },
+      { property: 'og:title', content: 'Suites' },
+      { property: 'og:description', content: 'A meta-framework that focuses on helping developers build solid test suites, eliminates boilerplate code, and improves their unit testing process.' },
+      { property: 'og:image', name: 'image', content: 'https://suites.dev/img/banner.png' },
+      { property: 'og:url', content: 'https://suites.dev' },
+      { property: 'og:type', content: 'website' }
     ],
     colorMode: {
       disableSwitch: true,
