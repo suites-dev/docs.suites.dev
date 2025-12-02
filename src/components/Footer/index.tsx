f5bd2f0099193f21306a640ac405d21156f05cc8
Iddan Aaronsohn <mail@aniddan.com>
2025-12-02 17:38:07 +0200

    Update the footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,23 +1,28 @@
-import React from 'react';
-import Link from '@docusaurus/Link';
-import { FaGithub } from 'react-icons/fa';
-import styles from './styles.module.css';
+import React from "react";
+import Link from "@docusaurus/Link";
+import { FaGithub } from "react-icons/fa";
+import styles from "./styles.module.css";
 
 export default function Footer(): JSX.Element {
   return (
     <footer className={styles.pageFooter}>
       <div className="container">
         <div className={styles.socialWrapper}>
-          <Link href="https://github.com/suites-dev/suites" target="_blank" className={styles.icon}>
-            <FaGithub/>
+          <Link
+            href="https://github.com/suites-dev/suites"
+            target="_blank"
+            className={styles.icon}
+          >
+            <FaGithub />
           </Link>
         </div>
         <p>
           Released under the MIT License
-          <br/>
-          Copyright © 2024 - <Link href="https://linkedin.com/in/omermorad" target="_blank">Omer Morad</Link>
-          <br/>
-          hosted by <Link href="https://www.netlify.com" target="_blank" rel="nofollow">Netlify</Link>
+          <br />
+          Copyright © 2025 -{" "}
+          <Link href="http://omermorad.me/" target="_blank">
+            Omer Morad
+          </Link>
         </p>
       </div>
     </footer>
